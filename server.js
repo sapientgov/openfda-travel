1604788cac624cb1298e864472359ca18a6c9411
Scott Magee <smagee@sapient.com>
2015-06-19 14:16:50 -0400

    updated server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ var SampleApp = function() {
         }
 
         //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
+        self.zcache['index.html'] = fs.readFileSync('./src/index.html');
     };
 
 
