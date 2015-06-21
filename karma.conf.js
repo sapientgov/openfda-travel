46ff31723a46f7fe5edb14d489c0345b20740caf
Christopher Perry <cperry@sapient.com>
2015-06-21 16:13:24 -0400

    fixed base path issue

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'src/test/',
+    basePath: 'test/',
 
 
     // frameworks to use
