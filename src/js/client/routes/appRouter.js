8663ef6b876902716bc6497b42c1b3db3c525a24
Christopher Perry <cperry@sapient.com>
2015-06-22 12:34:29 -0400

    adding drug label view

--- src/js/client/routes/appRouter.js
+++ src/js/client/routes/appRouter.js
@@ -3,20 +3,39 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
 Backbone.$ = $;
+var FdaService = require('../service/fdaService');
 
 //page view objects
 var DrugSearchPageView = require('../views/drug-search/drugSearchPageView');
+var DrugLabelInfoView = require('../views/labeling/drugLabelPageView');
 
 var AppRouter = Backbone.Router.extend({
     
     routes: {
-        '': 'drugSearch'
+        '': 'drugSearch',
+        'label/:brand': 'drugLabel'
     },
     
     drugSearch: function() {
         //initialize the drug search page view
         this.currentView = new DrugSearchPageView({el: $('.main-content')});
         this.currentView.render();
+    },
+    
+    drugLabel: function(brand) {
+        //find the drug we want
+        console.log('getting drug label info for %s.', brand);
+        FdaService.findLabelInfoByBrand(brand).done(function(data) {
+            //setup view
+            if(data.results && data.results.length > 0) {
+                this.currentView = new DrugLabelInfoView({drug: data.results[0]});
+                this.currentView.render();
+            } else {
+                console.log('no results returned');
+            }
+        }).fail(function() {
+            console.error('failed to find label by brand');
+        });
     }
 });
 
