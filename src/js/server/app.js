2c57108f7664d41b2116c334490d8e5fca4a612c
Christopher Perry <cperry@sapient.com>
2015-06-25 11:40:41 -0400

    added body parser to auth service

--- src/js/server/app.js
+++ src/js/server/app.js
@@ -3,6 +3,13 @@
 var express = require('express');
 var app = express();
 
+//add body parser support
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+
 //allow cross-site CORS requests (from: http://enable-cors.org/server_expressjs.html)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- src/js/server/auth/authService.js
+++ src/js/server/auth/authService.js
@@ -1,12 +1,26 @@
 'use strict';
 
+var $ = require('jquery');
 var express = require('express');
 var app = express();
+
+var https = require('https');
 
 module.exports = function(app){
     
     //login route
     app.post('/login', function (req, res) {
-      res.send('login response');
+        console.log('received login request.');
+        console.log('provider: %s', req.body.serviceProvider);
+        console.log('credentials: %s', req.body.credentials);
+        
+        //verify consumer ID is what we expect
+        
+        //verify provider is either api.twitter.com or www.digits.com
+        
+        //send verification request to provider to ensure they validated successfully
+        
+        
+        res.send('login response');
     });
 };
