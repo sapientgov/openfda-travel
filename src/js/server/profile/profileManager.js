1be13479e09dc354b28a4fd5dd08181dc01a2c57
umunroe <umunroe@sapient.com>
2015-06-26 16:43:06 -0400

    fixes to mongodb interface
    fixed the service interface fro doing updates and deletes.  Required
    adding the ‘son’ dependency to the app

--- src/js/server/profile/profileManager.js
+++ src/js/server/profile/profileManager.js
@@ -1,14 +1,32 @@
+
 'use strict';
+
+/**
+  * Defines the REST web service interface to access and manipulate profiles in
+  * a mongodb instance (profileProfider-mongodb.js)
+
+  code based on http://www.qat.com/rest-service-node-js-mongodb-express/  
+  */
 
 var mongoServer = 'localhost';
 var mongoPort = 27017;
 var ProfileManager = function(app) {
-
+ 
+  // Imports the MongoDB interface
   var ProfileProvider = require('./profileProvider-mongodb').ProfileProvider;
+
+  //Implements an instance of the MongoDB interface
   var profileProvider = new ProfileProvider(mongoServer, mongoPort);
 
 
+  /**
+    * RESTful GET method that defines the context and input to retrieve
+    * all profiles with the specified accountId
+    */
   app.get('/profiles/:accountId', function(req, res) {
+
+    //calls the MongoDB interface method with the reqest paramter that is expected
+    //to be the accountId
     profileProvider.fetchAllProfilesByAccountId(req.params.accountId, function(error, profiles) {
       if (error) {
         res.send(error, 500);
@@ -19,7 +37,13 @@ var ProfileManager = function(app) {
   });
 
 
+  /**
+    * RESTful POST method that defines the context and input to add a new
+    * profile document
+    */
   app.post('/profiles', function(req, res) {
+
+
     profileProvider.insertProfile(req.body, function(error, profile) {
       if (error) {
         res.send(error, 500);
@@ -29,35 +53,37 @@ var ProfileManager = function(app) {
     });
   });
 
-/*
-  app.get('/profiles/:id', function(req, res) {
-    profileProvider.fetchProfileById(req.params.id, function(error, profile) {
-      if (profile === null) {
-        res.send(error, 404);
-      } else {
-        res.send(profile);
-      }
-    });
-  });
-*/
 
-  app.post('/profiles/:id', function(req, res) {
+
+  /**
+    * RESTful PUT method that defines the context and input to update an
+    * existing profile document only if _id field matches the input 
+    * 'id' request parameter.
+    */
+  app.put('/profiles/:id', function(req, res) {
     var _profile = req.body;
     _profile._id = req.params.id;
+
     profileProvider.updateProfile(_profile, function(error, profile) {
       if (error) {
-        res.send(error, 404);
+        console.log(error);
+        res.status(404).send(error);
       } else {
-        res.send(profile);
+        res.send('');
       }
     });
   });
 
 
+  /**
+    * RESTful DELETE method that defines the context and input to remove
+    * a profile document only if _id field matches the input 
+    * 'id' request parameter.
+    */
   app.delete('/profiles/:id', function(req, res) {
     profileProvider.deleteProfile(req.params.id, function(error, profile) {
       if (error) {
-        res.send(error, 404);
+        res.status(404).send(error);
       } else {
         res.send('');
       }
