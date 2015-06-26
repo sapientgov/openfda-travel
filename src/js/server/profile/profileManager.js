4d54e93fbdc778b4d4649dcfdc9c5523a2097278
Christopher Perry <cperry@sapient.com>
2015-06-25 23:34:59 -0400

    added authentication checks to profile manager and fixed up account id handling

--- src/js/server/profile/profileManager.js
+++ src/js/server/profile/profileManager.js
@@ -2,67 +2,83 @@
 
 var mongoServer = 'localhost';
 var mongoPort = 27017;
-var ProfileManager = function(app) {
 
-  var ProfileProvider = require('./profileProvider-mongodb').ProfileProvider;
-  var profileProvider = new ProfileProvider(mongoServer, mongoPort);
+var AuthProvider = require('../auth/authProvider');
 
+function getAccountId(req, res) {
+    //read cookie
+    var token = req.cookies['fdaaw-token'];
+    if(AuthProvider.checkAuthentication(token)) {
+        return AuthProvider.getUserId(token);
+    } else {
+        //send unauthorized response
+        res.sendStatus(401);
+        return undefined;
+    }
+}
 
-  app.get('/profiles/:accountId', function(req, res) {
-    profileProvider.fetchAllProfilesByAccountId(req.params.accountId, function(error, profiles) {
-      if (error) {
-        res.send(error, 500);
-      } else {
-        res.send(profiles);
-      }
+var ProfileManager = function (app) {
+
+    var ProfileProvider = require('./profileProvider-mongodb').ProfileProvider;
+    var profileProvider = new ProfileProvider(mongoServer, mongoPort);
+
+    app.get('/profiles/', function (req, res) {
+        var accountId = getAccountId(req, res);
+        if(accountId) {
+            profileProvider.fetchAllProfilesByAccountId(accountId, function (error, profiles) {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    //TODO: remove accountId fields from results
+                    res.json(profiles);
+                }
+            });
+        }
     });
-  });
 
-
-  app.post('/profiles', function(req, res) {
-    profileProvider.insertProfile(req.body, function(error, profile) {
-      if (error) {
-        res.send(error, 500);
-      } else {
-        res.send(profile);
-      }
+    app.post('/profiles', function (req, res) {
+        var accountId = getAccountId(req, res);
+        if(accountId) {
+            
+            //add the account id to the data
+            var toAdd = req.body;
+            toAdd.accountId = accountId;
+            
+            //save the new profile
+            profileProvider.insertProfile(toAdd, function (error, profile) {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    res.json(profile);
+                }
+            });
+        }
     });
-  });
 
-/*
-  app.get('/profiles/:id', function(req, res) {
-    profileProvider.fetchProfileById(req.params.id, function(error, profile) {
-      if (profile === null) {
-        res.send(error, 404);
-      } else {
-        res.send(profile);
-      }
+    app.post('/profiles/:id', function (req, res) {
+        var accountId = getAccountId(req, res);
+        if(accountId) {
+            var _profile = req.body;
+            _profile._id = req.params.id;
+            profileProvider.updateProfile(_profile, function (error, profile) {
+                if (error) {
+                    res.status(404).send(error);
+                } else {
+                    res.json(profile);
+                }
+            });
+        }
     });
-  });
-*/
 
-  app.post('/profiles/:id', function(req, res) {
-    var _profile = req.body;
-    _profile._id = req.params.id;
-    profileProvider.updateProfile(_profile, function(error, profile) {
-      if (error) {
-        res.send(error, 404);
-      } else {
-        res.send(profile);
-      }
+    app.delete('/profiles/:id', function (req, res) {
+        profileProvider.deleteProfile(req.params.id, function (error, profile) {
+            if (error) {
+                res.status(404).send(error);
+            } else {
+                res.sendStatus(200);
+            }
+        });
     });
-  });
-
-
-  app.delete('/profiles/:id', function(req, res) {
-    profileProvider.deleteProfile(req.params.id, function(error, profile) {
-      if (error) {
-        res.send(error, 404);
-      } else {
-        res.send('');
-      }
-    });
-  });
 };
 
 exports.ProfileManager = ProfileManager;
