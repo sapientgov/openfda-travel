532e664adfcd61f6764e7b161a60571502a6199a
Christopher Perry <cperry@sapient.com>
2015-06-30 12:13:28 -0400

    additional button updates

--- src/js/client/views/landing/actionButtonsView.js
+++ src/js/client/views/landing/actionButtonsView.js
@@ -5,12 +5,31 @@ var Backbone = require('backbone');
 Backbone.$ = $;
 
 var ActionButtonsView = new Backbone.View.extend({
-    initialize: function(options) {
-        //supported values are LABEL, RECALL, APPROVED
-        this.view = options.view;
-    },
-    
-    render: function() {
+    el: '#action-buttons',
+    render: function(options) {
+        var view = options.view;
         
+        //show all buttons by default
+        this.$('.cta-btn').show();
+        
+        //turn off buttons as needed
+        if(view === 'LABEL') {
+            this.$('#action-btn-label').hide();
+        }
+        if(view === 'RECALL') {
+            this.$('#action-btn-recall').hide();
+        }
+        if(view === 'APPROVED') {
+            this.$('#action-btn-approved').hide();
+        }
+        
+        //update profile button text
+        //based on whether the current user is logged in and has a profile
+        var user = UserUtils.getCurrentUser();
+        if(user && user.get('loggedIn') && user.get('selectedPid')) {
+            this.$('#action-btn-profile').text('Add an additional profile');
+        } else {
+            this.$('#action-btn-profile').text('Personalize your results');
+        }
     }
 });
