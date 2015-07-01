24502943851dbb6d9175233024353869e7f45fe6
smagee-sapient <smagee@sapient.com>
2015-07-01 11:36:50 -0400

    Sorted Places Results By Distance
    Now uses max radius for all searches and just lists by distance.
    Changed to show only top 5 closest results. Fixed formatting.

--- src/js/client/views/location/mapModuleView.js
+++ src/js/client/views/location/mapModuleView.js
@@ -14,16 +14,16 @@ var _instance;
 var MapModuleView = Backbone.View.extend({
     el: '#map-module',
     locType: 'pharmacy',
-    
-    initialize: function(options) {
-        if(!options.location) {
+
+    initialize: function (options) {
+        if (!options.location) {
             throw new Error('Must supply user location');
         }
-        
+
         this.location = options.location;
     },
-    
-    render: function() {
+
+    render: function () {
         
         //show the map module elements
         this.$('.require-gps').slideDown();
@@ -37,8 +37,8 @@ var MapModuleView = Backbone.View.extend({
         }
         
         //Reference to this object because after jquery call, 'this' is different
-       var self = this;
-       //Function to get and hide places requests
+        var self = this;
+        //Function to get and hide places requests
         $('#map-module dt').click(function () {
             $(this).toggleClass('expanded');
             var locType = $(this).attr('data-locType');
@@ -49,8 +49,8 @@ var MapModuleView = Backbone.View.extend({
             }
         });
     },
-    
-    loadScript: function() {
+
+    loadScript: function () {
         //add listener for custom load event
         $(document).off('map-api-load').one('map-api-load', _.bind(this.initializeMap, this));
         
@@ -60,20 +60,20 @@ var MapModuleView = Backbone.View.extend({
         script.src = MAPS_API_URL + PLACES_API_URL + '&key=' + API_KEY + '&sensor=true&callback=mapApiLoaded';
         document.body.appendChild(script);
     },
-    
-    initializeMap: function() {
+
+    initializeMap: function () {
         //set map cetner
         var center = new google.maps.LatLng(this.location.coords.latitude, this.location.coords.longitude);
         var mapOptions = {
-            zoom : 14,
-            center : center,
-            mapTypeId : google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI : true,
+            zoom: 14,
+            center: center,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
             draggable: false
         };
         
         //create map object
-		this.map = new google.maps.Map(this.$('.map-ph').get(0), mapOptions);
+        this.map = new google.maps.Map(this.$('.map-ph').get(0), mapOptions);
         
         //add center marker
         var marker = new google.maps.Marker({
@@ -85,19 +85,19 @@ var MapModuleView = Backbone.View.extend({
         //Initialize Places
         this.initializePlaces();
     },
-    
-    initializePlaces: function(){
+
+    initializePlaces: function () {
         //Create the Places Service
         this.placesService = new google.maps.places.PlacesService(this.map);
     },
-    
-    placesRequest: function(locType){
+
+    placesRequest: function (locType) {
         var request = {
             location: new google.maps.LatLng(this.location.coords.latitude, this.location.coords.longitude), //Current Coordinates
-            radius: '2414', //1.5 miles = 2414 meters
-            types: [locType]
+            types: [locType],
+            rankBy: google.maps.places.RankBy.DISTANCE
         };
-        
+
         if (locType == 'embassy') {
             request.keyword = 'United States';
         }
@@ -113,39 +113,46 @@ var MapModuleView = Backbone.View.extend({
         //Make sure the service is working
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             var newcontent = '';
+            var i = 0;
+            var place;
             //Loop through the results
-            if (results.length >= 1) {
-                for (var i = 0; i < results.length; i++) {
-                    var place = results[i];
+            if (results.length < 5) {
+                for (i = 0; i < results.length; i++) {
+                    place = results[i];
                     newcontent += '<div><strong>' + place.name + '</strong><br/>' + place.vicinity + '</div>';
                 }
-            } 
+            } else {
+                for (i = 0; i < 5; i++) {
+                    place = results[i];
+                    newcontent += '<div><strong>' + place.name + '</strong><br/>' + place.vicinity + '</div>';
+                }
+            }
             //Append the results to the correct section, and animate their display
             $('dd.' + locType).html(newcontent).slideDown();
-        } else if (status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS){
+        } else if (status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
             var noResults = '<div><strong>No Results Found</strong></div>';
             //Append the No Result string to the correct section, and animate the display
             $('dd.' + locType).html(noResults).slideDown();
         }
     }
-                                         
+
 });
 
 var SingletonWrapper = {
-    createInstance: function(location) {
+    createInstance: function (location) {
         
         //setup map view
-        if(!_instance && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
+        if (!_instance && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
                 //init map module
                 console.log('initing map');
-                _instance = new MapModuleView({location: position});
+                _instance = new MapModuleView({ location: position });
                 _instance.render();
             });
         }
     },
-    
-    getInstance: function() {
+
+    getInstance: function () {
         return _instance;
     }
 };
